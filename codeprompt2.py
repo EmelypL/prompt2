a63f52f129bb9fcb756a08d0f4d4f14e018e1fe2
EmelypL <104232283+EmelypL@users.noreply.github.com>
2022-04-26 01:13:09 -0700

    session 2 prompt for coding journal 1

---
+++ codeprompt2.py
@@ -0,0 +1,16 @@
+codingprompt.py 
+
+x = 10.5
+y = 5.5
+z = 20
+
+sum = (x + y)
+print(type(sum))
+print("x + y = ", sum) #sum of two floating point integer
+difference =  (x-y)
+print(type(difference))
+print("x - y =", difference) #difference between two integers 
+
+product = (x * z)
+print(type(product))
+print("x * z =", product)
